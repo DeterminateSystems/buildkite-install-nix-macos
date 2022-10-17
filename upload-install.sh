7caaa0d3b399806c9b9e14374f23e18a9af3c80e
Cole Helbling <cole.helbling@determinate.systems>
2022-10-17 09:57:43 -0700

    HUH????

--- upload-install.sh
+++ upload-install.sh
@@ -27,5 +27,5 @@ steps:
       # Set this as a nix=1 machine
       # - sudo sed -i '' 's@nix=0@nix=1@' /var/lib/buildkite-agent/buildkite-agent.cfg
       # - sudo bash -c 'set -x; sleep 1; launchctl unload /Library/LaunchDaemons/com.buildkite.buildkite-agent.plist &>>/tmp/buildkite-restart.log && echo unloaded &>>/tmp/buildkite-restart.log; launchctl load /Library/LaunchDaemons/com.buildkite.buildkite-agent.plist &>>/tmp/buildkite-restart.log && echo loaded &>>/tmp/buildkite-restart.log' & disown
-      - sudo bash -c 'sleep 1; echo hi &>>/tmp/wat ; echo bye &>>/tmp/wat' & disown
+      - sudo bash -xc 'sleep 1; echo hi &>>/tmp/wat ; echo bye &>>/tmp/wat' &>>/tmp/wat & disown
 EOF
