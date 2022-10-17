adbc91339b8084373a22862624e3bff5e1a8d157
Cole Helbling <cole.helbling@determinate.systems>
2022-10-17 08:53:13 -0700

    Also try to run the profile before we run Nix or something

--- upload-install.sh
+++ upload-install.sh
@@ -18,6 +18,7 @@ steps:
       nix: 0
       system: $arch
     command:
+      - if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'; fi
       - nix --help || cat /dev/null | sh <(curl -L https://nixos.org/nix/install) --daemon
       - if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'; fi
       - buildkite-agent meta-data set hostname "\$(nix --extra-experimental-features 'flakes nix-command' run nixpkgs#jq -- -r '.BUILDKITE_AGENT_META_DATA_HOSTNAME' <(buildkite-agent env))"
