22c104bcf50b8f5e39fe9809ae118f16960b3199
Cole Helbling <cole.helbling@determinate.systems>
2022-10-17 08:41:24 -0700

    Maybe we can set -x and see what's happening

--- upload-install.sh
+++ upload-install.sh
@@ -18,8 +18,8 @@ steps:
       nix: 0
       system: $arch
     command:
+      - set -x
       - echo 'cat /dev/null | sh <(curl -L https://nixos.org/nix/install) --daemon'
-      - buildkite-agent env --pretty
       - buildkite-agent meta-data set hostname "\$BUILDKITE_AGENT_META_DATA_HOSTNAME"
       # - echo buildkite-agent meta-data set nix 1
       - ./upload-erase.sh $arch | buildkite-agent pipeline upload
