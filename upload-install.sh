1f267554f423b4e6f2938a47ba349c17c052e3c6
Cole Helbling <cole.helbling@determinate.systems>
2022-10-17 11:31:09 -0700

    Re-enable blocking and erase step

--- upload-install.sh
+++ upload-install.sh
@@ -7,10 +7,10 @@ arch="$1"
 
 cat <<EOF
 steps:
-  # - block: "$arch"
-  #   key: $arch-manual-intervention
+  - block: "$arch"
+    key: $arch-manual-intervention
   - label: "Test on $arch"
-    # depends_on: $arch-manual-intervention
+    depends_on: $arch-manual-intervention
     concurrency_group: $arch-install
     concurrency: 1
     agents:
@@ -23,7 +23,7 @@ steps:
       - nix --help &>/dev/null || cat /dev/null | sh <(curl -L https://nixos.org/nix/install) --daemon
       - if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'; fi
       - buildkite-agent meta-data set hostname "\$(nix --extra-experimental-features 'flakes nix-command' run nixpkgs#jq -- -r '.BUILDKITE_AGENT_META_DATA_HOSTNAME' <(buildkite-agent env))"
-      # - ./upload-erase.sh $arch | buildkite-agent pipeline upload
+      - ./upload-erase.sh $arch | buildkite-agent pipeline upload
       # Set this as a nix=1 machine
       - sudo sed -i '' 's@nix=0@nix=1@' /var/lib/buildkite-agent/buildkite-agent.cfg
       - echo please > /tmp/please-restart-buildkite-agent
